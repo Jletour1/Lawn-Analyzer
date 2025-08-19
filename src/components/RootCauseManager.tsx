0a843779e2395ec131794b72411a4d3e0eff2be4
Jletour1 <joshletourneau14@gmail.com>
2025-08-19 15:23:11 -0400

    Updated RootCauseManager.tsx

--- src/components/RootCauseManager.tsx
+++ src/components/RootCauseManager.tsx
@@ -1044,13 +1044,7 @@ const RootCauseManager: React.FC = () => {
                           </div>
                         ))}
                         <button
-                         onClick={() => {
-                           setSelectedRootCause(rootCause);
-                           setShowScheduleForm(true);
-                         }}
-                           setSelectedRootCause(rootCause);
-                           setShowScheduleForm(true);
-                         }}
+                          onClick={() => addSolution(rc.id)}
                           className="inline-flex items-center gap-2 px-3 py-1.5 text-sm rounded-lg bg-purple-50 text-purple-700 hover:bg-purple-100"
                         >
                           <Plus className="w-4 h-4" />
