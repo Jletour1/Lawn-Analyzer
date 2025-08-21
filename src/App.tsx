9d8231a0f110d76fa7e954c0b7c3e92dc1db845a
Jletour1 <joshletourneau14@gmail.com>
2025-08-20 23:33:16 -0400

    Replace AdminApp with exact UnifiedAdminDashboard code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Leaf, Camera, Brain, Target } from 'lucide-react';
-import AdminApp from './components/AdminApp';
+import UnifiedAdminDashboard from './components/UnifiedAdminDashboard';
 
 function App() {
   return (
     <Routes>
-      <Route path="/admin" element={<AdminApp />} />
+      <Route path="/admin" element={<UnifiedAdminDashboard />} />
       <Route path="/" element={<MainApp />} />
     </Routes>
   );
